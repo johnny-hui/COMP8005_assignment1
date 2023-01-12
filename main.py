b127f84f550117a18703169812e1bce041be0c88
johnny-hui <johnny_hui@live.com>
2023-01-12 06:43:43 -0500

    Implemented ability to read /etc/shadow file.

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import getopt
+import os
+import sys
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def check_if_root_user():
+    if not os.geteuid() == 0:
+        sys.exit("[+] ERROR: Only the 'root' user can run this script "
+                 "(Please run this script with sudo)...\n")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def parse_arguments():
+    # Remove file name from argument list
+    arguments = sys.argv[1:]
+
+    # Getting the file directory from (-f flag) and users (as args)
+    opts, user_list_args = getopt.getopt(arguments, 'f:')
+
+    for opt, argument in opts:
+        if opt == '-f':
+            file_directory = argument
+            print(file_directory)
+            return file_directory, user_list_args
+
+
+def check_if_file_exists():
+    # Check if path/file exists
+    try:
+        if not os.path.exists(file_directory):
+            sys.exit("[+] ERROR: File Doesn't Exist!\n")
+        else:
+            print("File Exists!\n")
+    except FileNotFoundError:
+        sys.exit("[+] ERROR: File Doesn't Exist!\n")
+
+
+# Main Driver
+if __name__ == "__main__":
+    check_if_root_user()
+    file_directory, user_list_args = parse_arguments()
+    check_if_file_exists()
+
+    print(user_list_args)
+
+    # Reading contents of the file
+    file = open(file_directory, 'r')
+    for line in file:
+        print(line)
